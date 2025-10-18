e7080d35bf71f2575befc78e46c77dff8a62deae
shem2004 <shemisaiah0@gmail.com>
2025-10-18 15:42:45 +0800

    Update backend code

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('login.html')   
+    return render_template('login.html')  
 
 @app.route('/register')
 def register_page():
