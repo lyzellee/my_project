c35158b81b69fd6140ee42e64430fcaa300a8320
shem2004 <shemisaiah0@gmail.com>
2025-10-20 15:53:48 +0800

    Updated flask set up

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-from flask import Flask, request, jsonify, render_template
-from dotenv import load_dotenv
-import os
-
-load_dotenv()  # load .env file
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -13,17 +9,6 @@ def home():
 @app.route('/register')
 def register_page():
     return render_template('register.html')
-
-@app.route('/api/login', methods=['POST'])
-def login():
-    data = request.get_json()
-    username = data.get("username")
-    password = data.get("password")
-    
-    if username == os.getenv('ADMIN_USER') and password == os.getenv('ADMIN_PASS'):
-        return jsonify({"status": "success", "message": "Login successful"})
-    else:
-        return jsonify({"status": "error", "message": "Invalid credentials"}), 401
 
 if __name__ == '__main__':
     app.run(debug=True)
